3c6ef9d1d109419589728b94c583a1e6aaacb949
auxghlann <orenoytemail@gmail.com>
2025-01-10 18:36:23 +0800

    feat: Add router function to get the generated response from the Mistral class

--- app/routers/rtr_mistral.py
+++ app/routers/rtr_mistral.py
@@ -4,11 +4,26 @@ from app.core.mistral import Mistral
 
 mistral_router = APIRouter(prefix="/chat")
 
+mistral_instance = Mistral()
+
 class Prompt(BaseModel):
     input: str
 
 @mistral_router.post("/prompt")
-def generate_response():
-    ...
+def generate_response(prompt: Prompt):
+
+    input: str = prompt.input
+
+    response: str = mistral_instance.generate_response(input)
+
+    if response:
+        return {
+            "message" : response
+        }
+
+    else:
+        return {
+            "message": "Could not generate response due to error"
+        }
 
 
