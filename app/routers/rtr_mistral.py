b907799f325ce26e8b0fb646f9bc1ecb8b6c390a
auxghlann <orenoytemail@gmail.com>
2025-01-16 20:40:19 +0800

    feat: Add try except case

--- app/routers/rtr_mistral.py
+++ app/routers/rtr_mistral.py
@@ -1,29 +1,28 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from app.core.mistral import Mistral
 
-mistral_router = APIRouter(prefix="/chat")
 
-mistral_instance = Mistral()
+mistral_router: APIRouter = APIRouter(prefix="/chat")
+
+mistral_instance: Mistral = Mistral()
 
 class Prompt(BaseModel):
     input: str
 
 @mistral_router.post("/prompt")
-def generate_response(prompt: Prompt):
+def generate_response(prompt: Prompt) -> dict:
 
-    input: str = prompt.input
-
-    response: str = mistral_instance.generate_response(input)
-
-    if response:
-        return {
-            "message" : response
-        }
-
-    else:
-        return {
-            "message": "Could not generate response due to error"
-        }
+    try:
+        input: str = prompt.input
+        response: str = mistral_instance.generate_response(input)
+        if response:
+            return {
+                "message": response
+            }
+        else:
+            raise HTTPException(status_code=500, detail="Could not generate response due to error")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
 
