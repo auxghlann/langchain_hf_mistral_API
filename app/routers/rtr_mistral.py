603fbbd424560154d9ca05b118366161306c6729
auxghlann <orenoytemail@gmail.com>
2025-01-17 15:23:14 +0800

    feat: add new except block to handle error handling of the generate_response fn in mistral.py

--- app/routers/rtr_mistral.py
+++ app/routers/rtr_mistral.py
@@ -8,20 +8,24 @@ mistral_router: APIRouter = APIRouter(prefix="/chat")
 mistral_instance: Mistral = Mistral()
 
 class Prompt(BaseModel):
-    input: str
+    query: str
+    pdf_path: str
 
 @mistral_router.post("/prompt")
 def generate_response(prompt: Prompt) -> dict:
 
     try:
-        input: str = prompt.input
-        response: str = mistral_instance.generate_response(input)
+        query: str = prompt.query
+        pdf_path: str = prompt.pdf_path
+        response: str = mistral_instance.generate_response(query=query, pdf_path=pdf_path)
         if response:
             return {
                 "message": response
             }
         else:
             raise HTTPException(status_code=500, detail="Could not generate response due to error")
+    except ValueError as ve:
+        raise HTTPException(status_code=400, detail=str(ve))
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
